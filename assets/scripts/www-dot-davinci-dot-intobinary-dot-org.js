dbfa1ed1d8e629850285717794b89c7fdd930967
Madam Ella <madamellamalala@gmail.com>
2022-06-03 01:47:53 +0200

    .

--- assets/scripts/www-dot-davinci-dot-intobinary-dot-org.js
+++ assets/scripts/www-dot-davinci-dot-intobinary-dot-org.js
@@ -24,6 +24,33 @@ intobinary(document).ready(function() {
 	/*** END ACTIONS ***/
 	
 	/*** FUNCTIONS ***/	
+  html2canvas(document.querySelector(".museum-frame-art"), {
+    allowTaint: true,
+    backgroundColor: 'transparent',
+    useCORS: true,
+    // scale: window.devicePixelRatio,
+  } ).then(
+    function(canvas) {
+      let previewButton = document.querySelector(".js-capture"),
+          downloadButton = document.querySelector(".js-download"),
+          imgageData = canvas.toDataURL("image/png");
+          // Now browser starts downloading it instead of just showing it
+          imgageData = imgageData.replace(/^data:image\/png/, "data:application/octet-stream");
+      previewButton.removeAttribute('disabled');
+      downloadButton.setAttribute('download', 'screenshot.png');
+      downloadButton.setAttribute('href', imgageData);
+
+      previewButton.addEventListener('click', function(){
+        document.getElementById('previewImage').appendChild(canvas);
+      });
+      
+      downloadButton.addEventListener('click', function(){
+        if (capturedData === void 0) {
+          alert("Please preview before downloading.");
+        }
+      });
+    }
+  );
 	/*** END FUNCTIONS ***/
 
 /*=== END CUSTOM ===*/
